d70b9147c7ea1c2423f04ab37ef213f0e6b83df4
Robert Xu <robxu9@gmail.com>
2013-07-27 00:18:07 -0400

    Migrate to using Eclipse AST
    Signed-off-by: Robert Xu <robxu9@gmail.com>

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ i18n-ast
 ========
 
 AST parser as a helper for i18n - scans source code, finds methods _ and tr, and dumps string into properties file for transifex.
+
+**Notice**: You'll have to use install-to-project-repo in order to have Maven resolve dependencies.
--- src/main/java/com/theminequest/i18n/Main.java
+++ src/main/java/com/theminequest/i18n/Main.java
@@ -1,18 +1,20 @@
 package com.theminequest.i18n;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
-import japa.parser.JavaParser;
-import japa.parser.ParseException;
-import japa.parser.ast.CompilationUnit;
-import japa.parser.ast.expr.Expression;
-import japa.parser.ast.expr.MethodCallExpr;
-import japa.parser.ast.visitor.VoidVisitorAdapter;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 
+// http://www.programcreek.com/2011/11/use-jdt-astparser-to-parse-java-file/
 public class Main {
 	
 	public static void _(String s, Object... args) {
@@ -32,7 +34,7 @@ public class Main {
 		_("Hello World 2!", 10, new Properties());
 	}
 	
-	public static void recurse(File current, Properties prop) throws ParseException, IOException {
+	public static void recurse(File current, Properties prop) throws IOException {
 		if (!current.exists())
 			return;
 		if (current.isFile()) {
@@ -44,25 +46,56 @@ public class Main {
 			recurse(f, prop);
 	}
 	
-	public static void parseCompilationUnit(final File sourceFile, final Properties prop) throws ParseException, IOException {
-		final CompilationUnit cu = JavaParser.parse(sourceFile);
-		cu.accept(new VoidVisitorAdapter<Void>() {
+	public static void parseCompilationUnit(final File sourceFile, final Properties prop) throws IOException {
+		System.out.printf("Processing %s\n", sourceFile.getCanonicalPath());
+		
+		parse(readFileToString(sourceFile), prop);
+		
+	}
+	
+	// read file content into a string
+	public static String readFileToString(File file) throws IOException {
+		StringBuilder fileData = new StringBuilder(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		
+		char[] buf = new char[10];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+		
+		reader.close();
+		
+		return fileData.toString();
+	}
+	
+	// use ASTParse to parse string
+	public static void parse(String str, final Properties prop) {
+		ASTParser parser = ASTParser.newParser(AST.JLS4);
+		parser.setSource(str.toCharArray());
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+		
+		cu.accept(new ASTVisitor() {
 			
 			@Override
-			public void visit(final MethodCallExpr n, final Void arg) {
-				
-				if (n.getName().equals("tr") || n.getName().equals("_")) {
-					List<Expression> arguments = n.getArgs();
+			public boolean visit(MethodInvocation node) {
+				if (node.getName().getIdentifier().equals("tr") || node.getName().getIdentifier().equals("_")) {
+					List<?> arguments = node.arguments();
 					String translate = arguments.get(0).toString();
-					if (!translate.startsWith("\"") || !translate.endsWith("\""))
-						throw new RuntimeException("Bad argument at " + sourceFile.getName() + ":" + n.getBeginLine() + "!");
+					if (translate.equals("translate")) // for I18NMessage
+						return super.visit(node);
 					translate = translate.substring(0, translate.length() - 1).substring(1);
 					prop.put(translate, translate);
 				}
-				
-				super.visit(n, arg);
+				return super.visit(node);
 			}
-		}, null);
+			
+		});
+		
 	}
 	
 }
